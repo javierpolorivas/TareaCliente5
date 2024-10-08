5ae4a95881f298fdfbba8e2a15ad411f65abaf27
Javier Polo <jpolo@uc-3-336.AluVedruna.local>
2024-10-08 20:00:31 +0200

    Tarea 05

---
+++ script.js
@@ -0,0 +1,4 @@
+temaAzul.addEventListener("click", () => document.body.style.backgroundColor = 'blue');
+temaRojo.addEventListener("click", () => document.body.style.backgroundColor = 'red');
+temaVerde.addEventListener("click", () => document.body.style.backgroundColor = 'green');
+resetTema.addEventListener("click", () => document.body.style.backgroundColor = 'black');
